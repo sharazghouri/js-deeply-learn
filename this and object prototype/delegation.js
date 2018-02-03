2917788b629c64bcda0d571ff7aa9fa20a7ee8d8
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-02-03 20:39:00 +0500

    OO delegation

--- this and object prototype/delegation.js
+++ this and object prototype/delegation.js
@@ -1,3 +1,24 @@
 /**
  * Created by welcom on 2/2/2018.
  */
+
+
+function Foo( eho ){
+
+     this.me = who;
+}
+Foo.prototype.identiify  = function () {
+    return " i am  "+ this.me;
+};
+function Bar( who ) {
+
+    Foo.call( this, who)
+}
+Bar.prototype.speak = function () {
+
+    alert( 'Hello' + this.identiify()+ '.');
+};
+var b1 = new Bar( 'b1');
+var b2 = new Bar( 'b2');
+b1.speak();
+b1.speak();g
