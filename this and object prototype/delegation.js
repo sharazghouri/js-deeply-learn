8180ee83696cf1fb775a0cabab2882e94df4182f
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-02-06 08:29:19 +0500

    get function parameter length

--- this and object prototype/delegation.js
+++ this and object prototype/delegation.js
@@ -2,23 +2,49 @@
  * Created by welcom on 2/2/2018.
  */
 
-
-function Foo( eho ){
+ //oo delegation
+/*
+function Foo( who ){
 
      this.me = who;
 }
-Foo.prototype.identiify  = function () {
+Foo.prototype.identify  = function () {
     return " i am  "+ this.me;
 };
 function Bar( who ) {
 
     Foo.call( this, who)
 }
+Bar.prototype = Object.create( Foo.prototype );
 Bar.prototype.speak = function () {
 
-    alert( 'Hello' + this.identiify()+ '.');
+    alert( 'Hello' + this.identify()+ '.');
 };
 var b1 = new Bar( 'b1');
 var b2 = new Bar( 'b2');
 b1.speak();
-b1.speak();g
+b2.speak();
+*/
+
+//0L00
+
+var Foo = {
+    init:function ( who ) {
+        this.me = who;
+    },
+    identify: function () {
+        return 'i am '+ this.me;
+    }
+}
+ var  Bar = Object.create( Foo );
+Bar.speak = function () {
+
+    alert( 'Hello' + this.identify() + '.');
+};
+var b1 = Object.create( Bar );
+b1.init( 'b1' );
+var b2 = Object.create( Bar );
+b2.init( 'b2' );
+
+b1.speak();
+b2.speak();
---
+++ type and grammers/ch1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by welcom on 2/6/2018.
+ */
+
+function  a( b ,c ) {
+
+}
+// will see the lenght of parameter in function object
+a.length;
