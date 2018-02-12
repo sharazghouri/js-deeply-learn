2df977c6e28fe4bf1ab63c27c7a32b7386e23865
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-02-12 20:37:14 +0500

    Mental Model Compared OOP

--- this and object prototype/delegation.js
+++ this and object prototype/delegation.js
@@ -1,5 +1,6 @@
 // Delegation Theory
 
+/*
 var Task = {
     setId : function ( ID ) {
          this.id = ID;
@@ -23,3 +24,54 @@ XYZ.outputTaskDetails = function ( ) {
     this.outputID();
     console.log( this.label  );
 }
+
+*/
+//Debug
+/*
+function Foo() {}
+
+var a1 = new Foo();
+
+a1; // Foo {}
+
+a1.constructor; //Foo(){}
+a1.constructor.name ; //Foo*/
+
+//OL-OO
+/*
+
+var Foo ={};
+var a1 = Object.create( Foo );
+console.log( a1 );
+Object.defineProperty( Foo, "constructor", {
+    enumerable: false,
+    value: function Gotcha(){}
+});
+
+console.log(  a1 ); // Gotcha {}
+
+*/
+//Mental Model Compared OOP
+function  Foo ( Who ) {
+    this.me = Who;
+    
+}
+Foo.prototype.identify = function () {
+
+    return ' I am ' + this.me;
+};
+
+function  Bar( who ) {
+    Foo.call( this , who);
+};
+Bar.prototype =     Object.create( Foo.prototype );
+
+Bar.prototype.speak = function() {
+    alert( "Hello, " + this.identify() + "." );
+};
+
+var b1 = new Bar( 'b1' );
+var b2 = new Bar( 'b2' );
+b1.speak();
+b2.speak();
+
