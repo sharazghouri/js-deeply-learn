2ef62b4ef74e710ecafb7879570783b880c2adf5
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-02-12 18:49:01 +0500

    Delegation Theory

--- this and object prototype/delegation.js
+++ this and object prototype/delegation.js
@@ -1,50 +1,25 @@
-/**
- * Created by welcom on 2/2/2018.
- */
+// Delegation Theory
 
- //oo delegation
-/*
-function Foo( who ){
-
-     this.me = who;
-}
-Foo.prototype.identify  = function () {
-    return " i am  "+ this.me;
-};
-function Bar( who ) {
-
-    Foo.call( this, who)
-}
-Bar.prototype = Object.create( Foo.prototype );
-Bar.prototype.speak = function () {
-
-    alert( 'Hello' + this.identify()+ '.');
-};
-var b1 = new Bar( 'b1');
-var b2 = new Bar( 'b2');
-b1.speak();
-b2.speak();
-*/
-
-//0L00
-
-var Foo = {
-    init:function ( who ) {
-        this.me = who;
+var Task = {
+    setId : function ( ID ) {
+         this.id = ID;
     },
-    identify: function () {
-        return 'i am '+ this.me;
-    }
-}
- var  Bar = Object.create( Foo );
-Bar.speak = function () {
-
-    alert( 'Hello' + this.identify() + '.');
+   outputID : function () {
+          console.log( this.id );
+   }
 };
-var b1 = Object.create( Bar );
-b1.init( 'b1' );
-var b2 = Object.create( Bar );
-b2.init( 'b2' );
 
-b1.speak();
-b2.speak();
+// make delegate to the `Task`
+
+var XYZ = Object.create( Task );
+
+XYZ.prepareTask = function ( ID , Label) {
+    this.setId( Id );
+    this.label = Label;
+    
+};
+XYZ.outputTaskDetails = function ( ) {
+
+    this.outputID();
+    console.log( this.label  );
+}
