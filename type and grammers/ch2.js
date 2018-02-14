91358f7efef4f8345e086344035ba73f91a58de1
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-02-14 08:20:39 +0500

    Void Can be use to return undefiend as result of expression

--- type and grammers/ch2.js
+++ type and grammers/ch2.js
@@ -6,12 +6,17 @@ console.log( 0.1 + 0.2 === 0.3 ); // false
 
 */
 
-function  Foo() {
+/*function  Foo() {
 
     // if strict mode this will not work but if we use var to declare and assing value this will work but this is bad coding practice
     undefined =3;
     console.log( undefined );
 
 }
-Foo();
+Foo();*/
+
+// Vlid use to return undefiend  as result of expersion
+var hag = 23;
+console.log( hag,   hag);
+console.log( hag,  void hag);
 
