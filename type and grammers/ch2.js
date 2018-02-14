132a47fab3b4a24c6a4d2f569b771eb1ed32b5aa
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-02-14 08:13:38 +0500

    Bad Coding Practice use undefiend as variable

--- type and grammers/ch2.js
+++ type and grammers/ch2.js
@@ -1,14 +1,17 @@
 
 
+/*
 console.log( 0.1 + 0.2 === 0.3 ); // false
 0.1+0.3 ; //0.30000000000000004
 
+*/
 
-// function  foo() {
-//
-//     undefined = 2;
-//
-//
-//
-// }
+function  Foo() {
+
+    // if strict mode this will not work but if we use var to declare and assing value this will work but this is bad coding practice
+    undefined =3;
+    console.log( undefined );
+
+}
+Foo();
 
