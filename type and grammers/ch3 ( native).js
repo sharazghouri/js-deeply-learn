0c0eec905bec90bff7624b53d125c125de552bf8
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-02-21 22:30:46 +0500

    Object Wrapper Gotchas

--- type and grammers/ch3 ( native).js
+++ type and grammers/ch3 ( native).js
@@ -10,7 +10,16 @@ Object.prototype.toString.call( s ); // "[object String]"
 
 
 //Boxing Wrappers
+/*
 
-var abc = "sharaz";
+var a = "sharaz";
 console.log( a.length ); // 3
-console.log( a.toUpperCase() ) // "ABC")
+console.log( a.toUpperCase() ) // "ABC")*/
+
+//Object Wrapper Gotchas
+var a = new Boolean( false );
+
+if (!a) {
+    console.log( "Oops" ); // never runs
+}
+
