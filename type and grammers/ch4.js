91adc8db4a99ec47da5a75a6da4a2cfcbefd047c
abc235896 <bc140401486@vu.edu.pk>
2018-02-27 15:52:44 +0500

    JSON.stringfy() with array;

--- type and grammers/ch4.js
+++ type and grammers/ch4.js
@@ -14,6 +14,7 @@ console.log( arr.toString() );// (4) [1, 3, 4, 5]
 
 //JSON Stringification  except that the serialization result is always a string:
 
+/*
  console.log(JSON.stringify( 42 ));      //"42"
  console.log(JSON.stringify( "42" ));    //""42""
  console.log(JSON.stringify( true ));    //"true"
@@ -24,6 +25,52 @@ console.log( JSON.stringify( undefined ) ); //undefined
 console.log( JSON.stringify( function () {} ) );//undefined
 console.log( JSON.stringify( [1,undefined,function(){},4] ) );//[1, null, null, 4]
 console.log( JSON.stringify( { a:2, b:function () {}} ) );//{a:2}
+*/
+/*
+
+var o={};
+
+var a={
+    b:42,
+    c:o,
+    d:function () {}
+};
+//create circular reference inside `a`
+o.e = a;
+//would through an error circular reference
+//JSON.stringify( a );
+//define custom value serialization
+a.toJSON= function () {
+    // only include `b` for serialization
+    return {b:this.b};
+};
+console.log( JSON.stringify( a ) );
+*/
+var a = {
+    val: [1,2,3],
+
+    // probably correct!
+    toJSON: function(){
+        return this.val.slice( 1 );
+    }
+};
+var b = {
+    val: [1,2,3],
+
+    // probably inorrect!
+    toJSON: function(){
+        return "["+ this.val.slice( 1 ).join() +"]";
+    }
+};
+//correct
+console.log( JSON.stringify( a ));//[2,3]
+//incorrect
+ console.log( JSON.stringify( b ));//"[2,3]"
+
+
+
+
+
 
 
 
