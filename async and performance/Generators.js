f68ac484162660ab5c583d2f3cbd8267c4b15b86
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-04-05 22:45:06 +0500

    Multiple iterator

--- async and performance/Generators.js
+++ async and performance/Generators.js
@@ -62,7 +62,7 @@ it.next();
  console.log( res );//30
 
   */
-
+/* 
   function*bar( x ){
     var y = x * ( yield  "hello");
    return y ;
@@ -73,3 +73,31 @@ it.next();
   console.log( res.value );
   var res = it.next (3);//  -- yield .. as an expression can send out messages in response to next(..) calls, 
   console.log( res.value );
+ */
+
+//Multiple iterator
+function * foo(){
+ var x = yield 2 ;
+ z++;
+ var y =yield ( x * z);
+ console.log( x, y , z );
+}
+var z = 1;
+var it1 = foo();
+var it2 = foo();
+
+var val1 = it1.next().value;
+console.log( 'val1', val1 ); // 2
+
+var val2 = it2.next().value;
+console.log( 'val2', val2 ); //2
+
+val1 = it1.next( val2 * 10 ).value; // 40   <-- x:20,  z:2
+console.log( 'val1', val1 ); // 2
+val2 = it2.next( val1 * 5 ).value;		// 600 <-- x:200, z:3
+console.log( 'val2', val2 ); //2
+it1.next( val2 / 2 );					// y:300 // 20 300 3
+console.log( 'val1', val1 ); // 2
+it2.next( val1 / 4 );					// y:10 // 200 10 3
+console.log( 'val2', val2 ); //2
+ 
