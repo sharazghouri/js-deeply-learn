51576af5f284a73c13a9d26f28147d5bb3c2a84f
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-04-08 14:57:14 +0500

    Default errot handing in Generator

--- async and performance/Generators.js
+++ async and performance/Generators.js
@@ -258,7 +258,7 @@ function * main(){
 var it  = main();
 it.next(); */
 
-function *main() {
+/* function *main() {
 	var x = yield "Hello World";
 
 	yield x.toLowerCase();	// cause an exception!
@@ -273,4 +273,23 @@ try {
 }
 catch (err) {
 	console.error( err );	// TypeError
+} */
+function *main() {
+	var x = yield "Hello World";
+
+	// never gets here
+	console.log( x );
+}
+
+var it = main();
+
+it.next();
+
+try {
+	// will `*main()` handle this error? we'll see!
+	it.throw( "Oops" );
+}
+catch (err) {
+	// nope, didn't handle it!
+	console.error( err );			// Oops
 }
