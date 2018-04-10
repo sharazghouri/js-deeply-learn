0cfc03224f2cf47be71a874163f4d7d96b97fac3
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-04-10 08:57:05 +0500

    Exceptions Delegated, Too!

--- async and performance/Generators.js
+++ async and performance/Generators.js
@@ -380,16 +380,16 @@ console.log(it.next().value );	// `*foo()` starting
 console.log(it.next().value );	// 4
 console.log(it.next().value );	// `*foo()` finished
 */
-
-function *foo() {
+// Delegation messages
+/* function *foo() {
 	console.log( "inside `*foo()2`:",  yield "B" );
 
 	console.log( "inside `*foo()3`:", yield "C" );
 
 	return "D";
 }
-
-function *bar() {
+ */
+/* function *bar() {
 	console.log( "inside `*bar()1`:", yield "A" );
 
 	// `yield`-delegation!
@@ -398,8 +398,8 @@ function *bar() {
 	console.log( "inside `*bar()5`:", yield "E" );
 
 	return "F";
-}
-
+} */
+/* 
 var it = bar();
 
 console.log( "outside:", it.next().value );//A
@@ -417,3 +417,61 @@ console.log( "outside:", it.next( 3 ).value );
 console.log( "outside:", it.next( 4 ).value );
 // inside `*bar()`: 4
 // outside: F
+ */
+//Exception delegation to 
+
+function *foo(){
+	try{
+		yield "B";
+	}
+	catch( err ){
+		console.log( "error caught inside `*foo()`:", err );
+	}
+	yield c;
+	throw d;
+}
+
+function *bar(){
+	yield 'A';
+
+	try{
+		yield *foo();
+	}catch( err ){
+		console.log( "error caught inside `*bar()`:", err );
+	}
+
+	yield "E";
+	yield *baz();
+
+//you can't get here
+	yield "G";
+}
+
+function *baz(){
+	throw "F";
+}
+
+var it = bar();
+
+
+console.log( "outside:", it.next().value );
+// outside: A
+
+console.log( "outside:", it.next( 1 ).value );
+// outside: B
+
+console.log( "outside:", it.throw( 2 ).value );
+// error caught inside `*foo()`: 2
+// outside: C
+
+console.log( "outside:", it.next( 3 ).value );
+// error caught inside `*bar()`: D
+// outside: E
+
+try {
+	console.log( "outside:", it.next( 4 ).value );
+}
+catch (err) {
+	console.log( "error caught outside:", err );
+}
+// error caught outside: F
