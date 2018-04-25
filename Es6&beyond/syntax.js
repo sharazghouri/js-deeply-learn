add8fefdd7e3253d92aab25053a9ce059d0d9cf1
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-04-25 22:13:39 +0500

    const learn

--- Es6&beyond/syntax.js
+++ Es6&beyond/syntax.js
@@ -16,19 +16,26 @@ console.log(a ,'ouside block'); */
   let b;
 
 } */
+// {
+// 	// `a` is not declared
+// 	if (typeof a === "undefined") {
+// 		console.log( "cool" );
+// 	}
+
+// 	// `b` is declared, but in its TDZ
+// 	if (typeof b === "undefined") {		// ReferenceError!
+//     // ..
+//     console.log( "cool" );
+// 	}
+
+// 	// ..
+
+// 	let b;
+// }
+//use of const
+
 {
-	// `a` is not declared
-	if (typeof a === "undefined") {
-		console.log( "cool" );
-	}
-
-	// `b` is declared, but in its TDZ
-	if (typeof b === "undefined") {		// ReferenceError!
-    // ..
-    console.log( "cool" );
-	}
-
-	// ..
-
-	let b;
+  const ASD ='asd';
+  console.log( ASD );
+  //ASD =3; //type error.
 }
