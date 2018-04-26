fc2534d88c809034713170cdf8e40917ebea8e6a
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-04-26 08:05:33 +0500

    spread / rest opreator in es

--- Es6&beyond/syntax.js
+++ Es6&beyond/syntax.js
@@ -40,25 +40,70 @@ console.log(a ,'ouside block'); */
 //   //ASD =3; //type error.
 // }
 //Block scoped functions
-{
+// {
 
-  foo();// opsoite to let
+//   foo();// opsoite to let
 
-  function foo(){
+//   function foo(){
 
-  }
+//   }
+// }
+// //foo(); //es 6 will be refernce error
+
+// if (true) {
+// 	function foo() {
+// 		console.log( "1" );
+// 	}
+// }
+// else {
+// 	function foo() {
+// 		console.log( "2" );
+// 	}
+// }
+
+// foo();
+//Spread/Rest Operator
+// function foo( x, y ,z){
+// console.log( x,y,z);
+// }
+// foo(...[1,2,3]);
+
+// //exapnd value
+// var a = [2,3,4];
+// var b= [1, ...a,5];
+// console.log(b);
+// //get and set falue in array using function call
+// function bar( x, y, ...z){
+//   console.log(x,y,z);
+// }
+// bar(1,2,3,4,5);
+// //bar(1,2,3,{a:3,b:4});
+
+// function sar( ...args ){
+//   console.log(args);
+// }
+// sar(2,3,4,5,6);
+
+
+function foo( ...args ){
+
+  args.shift();//discarding furst element
+  console.log( ...args);
 }
-//foo(); //es 6 will be refernce error
 
-if (true) {
-	function foo() {
-		console.log( "1" );
-	}
-}
-else {
-	function foo() {
-		console.log( "2" );
-	}
-}
+//old way to use ...operator
+function bar(){
+  //turun arguments into reak arral
+  var args= Array.prototype.slice.call( arguments);
 
-foo();
+  args.push( 4, 5);
+  // filter out odd numbers
+	args = args.filter( function(v){
+		return v % 2 == 0;
+  } );  
+
+  // pass along all of `args` as arguments
+	// to `foo(..)`
+	foo.apply( null, args );
+}
+bar(0,1,2,3,4) ;
