cc00b4a162cf02cc4d1b7b554e8f5873691b8f0e
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-04-28 21:49:16 +0500

    Desctructring paramenter

--- Es6&beyond/syntax.js
+++ Es6&beyond/syntax.js
@@ -286,6 +286,70 @@ function foo() {
 // console.log( d,w); // undefiend undefiend
 
 
-var a = [2, 3, 4];
-var [b, ...c] = a;
-console.log(b, c);
+// var a = [2, 3, 4];
+// var [b, ...c] = a;
+// console.log(b, c);
+//Default Value Assignment
+// var [ a = 3, b = 6, c = 9, d = 12 ] = foo();
+// var { x = 5, y = 10, z = 15, w = 20 } = bar();
+
+// console.log( a, b, c, d );			// 1 2 3 12
+// console.log( x, y, z, w );			// 4 5 6 20
+// //You can combine the default value assignment with the alternative assignment expression syntax covered earlier. For example:
+
+// var { x, y, z, w: WW = 20 } = bar();
+
+// console.log( x, y, z, WW );
+
+//easiler to under stand
+
+// var X = 300, Y = 200, Z = 100;
+// var o1 = { x: { y: 42 }, z: { y, z } };
+// ({ y: x = { y: y } } = o1);
+// ({ z: y = { y: z } } = o1);
+// ({ x: z = { y: x } } = o1);
+
+//Nested Destrucring 
+// var a1 = [1, [2, 3, 4], 5];
+// var o1 = { x: { y: { z: 6 } } };
+// var [a, [b, c, d], e] = a1;
+// var { x: { y: { z: w } } } = o1;
+// console.log(a, b, c, d, e); //1,2,3,4,5
+// console.log(w);
+// //Nested destructuring can be a simple way to flatten out object namespaces. For example:
+// var App = {
+// 	model: {
+// 		User: function(){  console.log('hello') }
+// 	}
+// };
+
+// // instead of:
+// // var User = App.model.User;
+
+// var { model: { User } } = App;
+
+// console.log(User);
+
+//Destructing para meter
+// function foo( [ x, y ] ) {
+// 	console.log( x, y );
+// }
+
+// foo( [ 1, 2 ] );					// 1 2
+// foo( [ 1 ] );						// 1 undefined
+// foo( [,2] );  //  undefined 1
+// foo( [ ] );  //  undefined  undefined
+
+// function foo({ x, y }) {
+//   console.log(x, y);
+// }
+// foo({ x: 3, y: 4 });
+// foo({x:3});
+// foo({y:4});
+// foo({});
+// function f3([ x, y, ...z], ...w) {
+// 	console.log( x, y, z, w );
+// }
+
+// f3( [] );							// undefined undefined [] []
+// f3( [1,2,3,4], 5, 6 );		
