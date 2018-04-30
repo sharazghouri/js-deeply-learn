232064076412f3e8aca3c3e77d4ccc4bfc076225
Sharaz Ghouri <sharazghouri235@outlook.com>
2018-04-30 21:32:35 +0500

    setter and getter

--- Es6&beyond/syntax.js
+++ Es6&beyond/syntax.js
@@ -461,3 +461,56 @@ config.log = config.log || {};
 //   y() { },
 // }
 // console.log( o);
+//Consisley unnamed
+
+function runSomtething(o) {
+  var x = Math.random();
+  var y = Math.random();
+
+  return o.something(x, y);
+
+}
+
+// runSomtething({
+//   something:function something(x, y){
+//     if(x > y){
+//       // recursively call with `x`
+// 			// and `y` swapped
+// 			return something( y, x );
+//     }
+
+//     return y-x;
+//   }
+// });
+
+// runSomtething({
+//   something: function something (x, y){
+//     if(x > y){
+//       // recursively call with `x`
+// 			// and `y` swapped
+// 			return something( y, x );
+//     }
+//     return y-x;
+//   }
+// });
+//setter and getter
+var o = {
+  __id: 10,
+  get  id () {
+      return this.__id++;
+  
+  },
+  //set id( { id : v = 0} ){
+  set id( v ){
+    this.__id = v;
+
+  }
+};
+
+console.log( o.id ); //10
+console.log( o.id ); //11
+ o.id =20;
+ console.log( o.id ); //20
+ //and
+ console.log( o.id ); //21
+ console.log( o.id ); //21 --still
